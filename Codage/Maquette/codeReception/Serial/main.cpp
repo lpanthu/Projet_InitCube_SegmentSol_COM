757aaa1722cf689406df48140784e390cfcc63d1
gaboulanger <gaboulanger@btslivh.eu>
2020-05-05 14:13:07 +0200

    Création du fichier main pour le code principal

---
+++ Codage/Maquette/codeReception/Serial/main.cpp
@@ -0,0 +1,25 @@
+/* BOULANGER GABRIELLE-ANNE
+ * Code main / principal
+ * fait le 05/05/2020
+ */
+/// Inclusions de toutes les classes ///
+	#include "Trame.cpp"
+	#include "Payload.cpp"
+	#include "PayloadMesure.cpp"
+	#include "PayloadMission.cpp"
+	#include "PayloadStatut.cpp"
+	#include "Trame.cpp"
+/// Inclusions de Biliothèques ///
+	#include <iostream>
+	#include <string.h>
+	#include <stdlib.h>
+	#include <stdio.h>
+///  Code Principal ///
+
+using namespace std;
+
+int main(){
+	
+	
+	return 0;
+}
